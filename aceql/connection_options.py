034b31ed5607b3b936eca76ce84b53153346a2b3
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-04-11 22:21:06 +0200

    ConnectionOptions update

--- aceql/connection_options.py
+++ aceql/connection_options.py
@@ -1,3 +1,22 @@
+#
+# This file is part of AceQL Python Client SDK.
+# AceQL Python Client SDK: Remote SQL access over HTTP with AceQL HTTP.
+# Copyright (C) 2020,  KawanSoft SAS
+# (http://www.kawansoft.com). All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
 from aceql import ProxyAuth
 
 
@@ -6,8 +25,8 @@ class ConnectionOptions(object):
 
     def __init__(self, *, proxies: dict = None, auth: ProxyAuth = None, password_is_session_id: bool = False,
                  gzip_result: bool = False, timeout: int = 0, request_headers: dict = None):
-        self.__proxies = proxies;
-        self.__auth = auth;
+        self.__proxies = proxies
+        self.__auth = auth
         self.__password_is_session_id = password_is_session_id
         self.__gzip_result = gzip_result
         self.__timeout = timeout
