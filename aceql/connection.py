13da9027bf4b6ca39e878ad74f54d9032aa072d2
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2021-04-13 20:01:45 +0200

    TestAll is now in tests directory

--- aceql/connection.py
+++ aceql/connection.py
@@ -20,6 +20,7 @@ from aceql._private.aceql_http_api import AceQLHttpApi
 from aceql.cursor import Cursor
 from aceql.connection_options import ConnectionOptions
 from aceql.proxy_auth import ProxyAuth
+from aceql.progress_indicator import ProgressIndicator
 
 
 class Connection(object):
@@ -59,17 +60,21 @@ class Connection(object):
             raise TypeError("database is null!")
 
         self.__aceQLHttpApi = AceQLHttpApi(url, username, password, database, connection_options)
+        self.__connection_options = connection_options
 
     @property
-    def _get_aceql_http_api(self):
+    def _get_aceql_http_api(self) -> AceQLHttpApi:
         return self.__aceQLHttpApi
 
-    def cursor(self):
+    def getConnectionsOptions(self) -> ConnectionOptions:
+        return self.____connection_options
+
+    def cursor(self) -> Cursor:
         """Instantiates and returns a cursor."""
         cursor = Cursor(self, self.__aceQLHttpApi)
         return cursor
 
-    def add_request_headers(self, headers):
+    def add_request_headers(self, headers: dict):
         """Allows to pass a dictionary of headers to each request."""
         self.__aceQLHttpApi.add_request_headers(headers)
 
@@ -77,15 +82,15 @@ class Connection(object):
         """Resets the request headers. The previously added headers with add_request_headers will be suppressed."""
         self.__aceQLHttpApi.reset_request_headers()
 
-    def set_progress_indicator(self, progress_indicator):
+    def set_progress_indicator(self, progress_indicator: ProgressIndicator):
         """ Allows to set a progress indicator."""
         self.__aceQLHttpApi.set_progress_indicator(progress_indicator)
 
-    def get_progress_indicator(self):
+    def get_progress_indicator(self) -> ProgressIndicator:
         """Returns the progress indicator in use."""
         return self.__aceQLHttpApi.get_progress_indicator()
 
-    def set_auto_commit(self, auto_commit):
+    def set_auto_commit(self, auto_commit: bool):
         """Sets this connection's auto-commit mode to the given state."""
         self.__aceQLHttpApi.set_auto_commit(auto_commit)
 
@@ -105,21 +110,21 @@ class Connection(object):
         """Print empty line on trace."""
         self.__aceQLHttpApi.trace("")
 
-    def trace(self, s):
+    def trace(self, s: str):
         """Prints the string on trace."""
         self.__aceQLHttpApi.trace(s)
 
     @staticmethod
-    def _is_trace_on():
+    def _is_trace_on() -> bool:
         """Says if trace is on."""
         return AceQLHttpApi.is_trace_on()
 
     @staticmethod
-    def _set_trace_on(trace_on):
+    def _set_trace_on(trace_on: bool):
         """Sets the trace on/off."""
         AceQLHttpApi.set_trace_on(trace_on)
 
-    def get_server_version(self):
+    def get_server_version(self) -> str:
         """Gets the server version of AceQL HTTP."""
         return self.__aceQLHttpApi.get_server_version()
 
@@ -131,7 +136,7 @@ class Connection(object):
         """Closes all the connection to the remote database and closes the HTTP session."""
         self.__aceQLHttpApi.logout()
 
-    def get_transaction_isolation(self):
+    def get_transaction_isolation(self) -> str:
         """Returns the current transaction isolation level."
 
            Will be one of the following constants:
@@ -143,34 +148,34 @@ class Connection(object):
         """
         return self.__aceQLHttpApi.get_transaction_isolation()
 
-    def set_transaction_isolation(self, level):
+    def set_transaction_isolation(self, level: str):
         """Sets the transaction isolation level."""
         self.__aceQLHttpApi.set_transaction_isolation(level)
 
-    def get_holdability(self):
+    def get_holdability(self) -> str:
         """Returns the holdability.
          One of hold_cursors_over_commit or close_cursors_at_commit.
         """
         return self.__aceQLHttpApi.get_holdability()
 
-    def set_holdability(self, holdability):
+    def set_holdability(self, holdability: str):
         """Sets the holdability."""
         self.__aceQLHttpApi.set_holdability(holdability)
 
-    def is_read_only(self):
+    def is_read_only(self) -> bool:
         """Says if Connection is read-only."""
         return self.__aceQLHttpApi.is_read_only()
 
-    def set_read_only(self, read_only):
+    def set_read_only(self, read_only: bool):
         """Allows to put Connection read-only mode."""
         self.__aceQLHttpApi.set_read_only(read_only)
 
     @staticmethod
-    def get_client_version():
+    def get_client_version() -> str:
         """Gets the SDK version."""
         return AceQLHttpApi.get_client_version()
 
     @staticmethod
-    def get_client_version_full():
+    def get_client_version_full() -> str:
         """Gets the SDK version with the Python version."""
         return AceQLHttpApi.get_client_version_full()
