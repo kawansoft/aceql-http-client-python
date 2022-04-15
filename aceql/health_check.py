59449c0e550af57541ae530d69c9b05f1ad53359
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2022-04-15 22:23:19 +0200

    Implement HealthCheck & HealthCheckTest

--- aceql/health_check.py
+++ aceql/health_check.py
@@ -23,15 +23,7 @@ import requests
 from requests import Request
 
 from aceql import Connection, Error
-from aceql._private.aceql_debug import AceQLDebug
-from aceql._private.aceql_execution_util import AceQLExecutionUtil
-from aceql._private.dto.table_names_dto import TableNamesDto
-from aceql._private.dto.jdbc_database_meta_data_dto import JdbcDatabaseMetaDataDto
-from aceql._private.dto.table_dto import TableDto
-from aceql._private.file_util import FileUtil
 from aceql._private.result_analyzer import ResultAnalyzer
-from aceql.metadata.table import Table
-from aceql.metadata.jdbc_database_meta_data import JdbcDatabaseMetaData
 
 
 class HealthCheck(object):
@@ -49,10 +41,10 @@ class HealthCheck(object):
             url: str = self.__connection.get_url();
             result = self.__aceql_http_api.call_with_get_url(url)
 
-            result_analyzer = ResultAnalyzer(result, self.__http_status_code)
+            result_analyzer = ResultAnalyzer(result, self.__aceql_http_api.get_http_status_code)
             if not result_analyzer.is_status_ok():
                 raise Error(result_analyzer.get_error_message(),
-                            result_analyzer.get_error_type(), None, None, self.__http_status_code)
+                            result_analyzer.get_error_type(), None, None, self.__aceql_http_api.get_http_status_code)
 
             return True
 
@@ -61,7 +53,7 @@ class HealthCheck(object):
             #     raise
             # else:
             #     raise Error(str(e), 0, e, None, self.__http_status_code)
-            self.__error = Error(str(e), 0, e, None, self.__http_status_code)
+            self.__error = Error(str(e), 0, e, None, self.__aceql_http_api.get_http_status_code())
             return False
 
     def get_error(self) -> Error:
@@ -77,7 +69,7 @@ class HealthCheck(object):
         end = int(round(time.time() * 1000))
         return end - begin
 
-    def get_response_time_select_1(self, sql: str) -> int:
+    def get_response_time_select_1(self) -> int:
         """Gets the response time of a "select 1" called on the remote database defined by the underlying
         Connection. """
         return self.get_response_time("select 1");
---
+++ tests/health_check_tests/health_check_test.py
@@ -0,0 +1,46 @@
+#
+# This file is part of AceQL Python Client SDK.
+# AceQL Python Client SDK: Remote SQL access over HTTP with AceQL HTTP.
+# Copyright (C) 2021,  KawanSoft SAS
+# (http://www.kawansoft.com). All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+from datetime import datetime
+
+from aceql import Connection, Cursor, HealthCheck
+from tests.dml.sql_delete_test import SqlDeleteTest
+from tests.util.connection_builder import ConnectionBuilder
+
+
+class HealthCheckTest(object):
+    """Delete all rows of customer and orderlog tables."""
+
+    def __init__(self, connection: Connection):
+        self.__connection = connection
+
+    def execute(self):
+        health_check = HealthCheck(self.__connection)
+        print("health_check.ping()                      : " + str(health_check.ping()))
+        print("health_check.get_response_time_select_1(): " + str(health_check.get_response_time_select_1()))
+        print(str(datetime.now()) + " End.")
+
+
+if __name__ == '__main__':
+    connection: Connection = ConnectionBuilder.get_connection()
+    try:
+        health_check_test = HealthCheckTest(connection)
+        health_check_test.execute()
+    finally:
+        connection.close()
+    exit()
