7789d4b3ddc1508cc8de1a6adc111c8ec1b2640d
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-08-12 21:45:20 +0200

    Version Values

--- aceql/_private/http_client_with_retry.py
+++ aceql/_private/http_client_with_retry.py
@@ -4,18 +4,18 @@ from requests.exceptions import RequestException, Timeout
 
 
 class HTTPClientWithRetry:
-    MAX_RETRIES = 0
-    RETRY_INTERVAL_MS = 0
+    _MAX_RETRIES = 0
+    _RETRY_INTERVAL_MS = 0
 
     @staticmethod
     def set_max_retries(max_retries):
         """Sets the maximum number of retries."""
-        HTTPClientWithRetry.MAX_RETRIES = max_retries
+        HTTPClientWithRetry._MAX_RETRIES = max_retries
 
     @staticmethod
     def set_retry_interval_ms(retry_interval_ms):
         """Sets the retry interval in milliseconds."""
-        HTTPClientWithRetry.RETRY_INTERVAL_MS = retry_interval_ms
+        HTTPClientWithRetry._RETRY_INTERVAL_MS = retry_interval_ms
 
     @staticmethod
     def get(url, **kwargs):
@@ -27,9 +27,9 @@ class HTTPClientWithRetry:
                 return response
             except (RequestException, Timeout) as e:
                 attempt += 1
-                if attempt > HTTPClientWithRetry.MAX_RETRIES:
+                if attempt > HTTPClientWithRetry._MAX_RETRIES:
                     raise e
-                time.sleep(HTTPClientWithRetry.RETRY_INTERVAL_MS / 1000.0)  # Convert ms to seconds
+                time.sleep(HTTPClientWithRetry._RETRY_INTERVAL_MS / 1000.0)  # Convert ms to seconds
 
     @staticmethod
     def post(url, data=None, json=None, **kwargs):
@@ -41,6 +41,6 @@ class HTTPClientWithRetry:
                 return response
             except (RequestException, Timeout) as e:
                 attempt += 1
-                if attempt > HTTPClientWithRetry.MAX_RETRIES:
+                if attempt > HTTPClientWithRetry._MAX_RETRIES:
                     raise e
-                time.sleep(HTTPClientWithRetry.RETRY_INTERVAL_MS / 1000.0)  # Convert ms to seconds
+                time.sleep(HTTPClientWithRetry._RETRY_INTERVAL_MS / 1000.0)  # Convert ms to seconds
